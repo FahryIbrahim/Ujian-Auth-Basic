77127e66b873073ef4049301785525b49ba3ffc4
Fahry Ibrahim <fahryibrahim06@gmail.com>
2024-08-25 00:30:21 +0700

    coba

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
+use Illuminate\Support\Facades\Hash;
 
 class ProfileController extends Controller
 {
@@ -15,6 +16,18 @@ class ProfileController extends Controller
     {
         // Task: fill in the code here to update name and email
         // Also, update the password if it is set
+        $data = $request->validated();
+        if ($data) {
+            $user = auth()->user();
+            $user->name = $request->input('name');
+            $user->email = $request->input('email');
+            
+            if ($request->has('password')) {
+                $user->password = Hash::make($request->input('password'));
+            }
+            
+            $user->save();
+        }
 
         return redirect()->route('profile.show')->with('success', 'Profile updated.');
     }
